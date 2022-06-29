c245bada0146ec40f178ce988ba8aeb301f08e0d
Vincent Knoop Pathuis <git@vpathuis.dds.nl>
2022-06-29 21:27:12 +0200

    Add file interface and some refactoring

---
+++ file_interface.py
@@ -0,0 +1,33 @@
+import csv
+
+from datetime import datetime
+
+from birthday import Birthday
+from birthday_list import assemble_birthday_list
+
+
+def read_jarig_file() -> list:
+    with open('jarig.csv') as csv_file:
+        birthdays = []
+        csv_reader = csv.DictReader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            birthday = row["birthday"].strip().split('-')
+            day = int(birthday[0])
+            month = int(birthday[1])
+            year = None
+            if len(birthday) > 1:
+                if birthday[2] != '1900':
+                    year = int(birthday[2])
+            birthdays.append(Birthday(name=row["name"], day=day, month=month, year=year))
+            line_count += 1
+    return birthdays
+
+
+birthdays = read_jarig_file()
+print(birthdays)
+today = datetime(2022, 6, 29)
+party_list = assemble_birthday_list(birthdays, today, 20, 10)
+
+for party in party_list:
+    print(party['name'], party['party_date'], party['age'] if 'age' in party else "?")
--- test_birthday_list.py
+++ test_birthday_list.py
@@ -1,32 +1,35 @@
 import unittest
 from datetime import datetime
 
-from birthday_list import BirthdayList
+from birthday_list import assemble_birthday_list
 from birthday import Birthday
 
 
 class BirthdayListTestCase(unittest.TestCase):
     def test_birthday_list(self):
-        bd1 = Birthday(year=1975, month=12, day=1)
-        bd2 = Birthday(year=1975, month=12, day=2)
-        bd3 = Birthday(month=12, day=3)
+        bd1 = Birthday(name='Aad', year=1975, month=12, day=1)
+        bd2 = Birthday(name='Bert', year=1975, month=12, day=2)
+        bd3 = Birthday(name='Claudia', month=12, day=3)
         bdl = [bd1, bd2, bd3]
 
-        bla = BirthdayList(bdl)
-        self.assertEqual(Birthday(day=1, month=12, year=1975), bla[0])
-        self.assertEqual(Birthday(day=2, month=12, year=1975), bla[1])
-        self.assertEqual(Birthday(day=3, month=12), bla[2])
+        self.assertEqual(Birthday(name='Aad', day=1, month=12, year=1975), bdl[0])
+        self.assertEqual(Birthday(name='Bert', day=2, month=12, year=1975), bdl[1])
+        self.assertEqual(Birthday(name='Claudia', day=3, month=12), bdl[2])
 
     def test_birthday_list_near_birthdays(self):
-        bd1 = Birthday(year=1975, month=12, day=1)
-        bd2 = Birthday(year=1975, month=12, day=2)
-        bd3 = Birthday(month=12, day=3)
+        bd1 = Birthday(name='Aad', year=1975, month=12, day=1)
+        bd2 = Birthday(name='Bert', year=1975, month=12, day=2)
+        bd3 = Birthday(name='Claudia', month=12, day=3)
         bdl = [bd1, bd2, bd3]
+
         today = datetime(2022, 12, 4)
-        bla = BirthdayList(bdl)
-        near_birthdays = bla.near_birthdays(today, 2, 2)
-        self.assertEqual(Birthday(day=2, month=12, year=1975), near_birthdays[0])
-        self.assertEqual(Birthday(day=3, month=12), near_birthdays[1])
+        near_birthdays = assemble_birthday_list(bdl, today, 2, 2)
+        self.assertEqual('Bert', near_birthdays[0]['name'])
+        self.assertEqual(datetime(2022, 12, 2).date(), near_birthdays[0]['party_date'])
+        self.assertEqual(47, near_birthdays[0]['age'])
+        self.assertEqual('Claudia', near_birthdays[1]['name'])
+        self.assertEqual(datetime(2022, 12, 3).date(), near_birthdays[1]['party_date'])
+        self.assertNotIn('age', near_birthdays[1])
 
 
 if __name__ == "__main__":
